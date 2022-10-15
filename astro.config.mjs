9ac897ff1ad3fef42dc0e68f480fcd7002a29da0
Edgar Barrios <edbarrios5@hotmail.com>
2022-10-14 20:58:46 -0500

    otro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import svelte from '@astrojs/svelte';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: node(),
-	integrations: [svelte()],
+	adapter: cloudflare(),
+	integrations: [svelte()]
 });
